e335e83d03ae51e882cb375160edc4ec5be12d40
marcelomaurinmartins <marcelomaurinmartins@gmail.com>
2019-09-16 12:34:35 -0300

    painel

--- Painel/js/painel.js
+++ Painel/js/painel.js
@@ -1,8 +1,26 @@
-var app = angular.module('painel', []);
+var app = angular.module('painel', ['io.service']);
+ app.run(function (io) {
+        io.init({
+            ioServer: 'http://localhost:3000',
+            apiServer: 'http://localhost:8080/api',
+            ioEvent: 'io.response'
+        });
+    });
 app.controller('myCtrl', function($scope) {
 	
   //this.IP = getIp(this.IP);
   this.IP = '127.0.0.1';
   
+      $scope.$watch('question', function (newValue, oldValue) {
+            if (newValue != oldValue) {
+                io.emit({item: 'question', newValue: newValue, oldValue: oldValue});
+            }
+        });
+ 
+        io.watch('answer', function (data) {
+            $scope.answer = data.value;
+            $scope.$apply();
+        });
+
   
 });
