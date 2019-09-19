8771412255176c8e466a3b3f7f8d466ae78c5763
marcelomaurinmartins <marcelomaurinmartins@gmail.com>
2019-09-19 12:50:46 -0300

    ok

--- Painel/js/funcs.js
+++ Painel/js/funcs.js
@@ -1,39 +1,14 @@
 
-
-function getIp(callback)
-{
-    function response(s)
-    {
-        callback(window.userip);
-
-        s.onload = s.onerror = null;
-        document.body.removeChild(s);
-    }
-
-    function trigger()
-    {
-        window.userip = false;
-
-        var s = document.createElement("script");
-        s.async = true;
-        s.onload = function() {
-            response(s);
-        };
-        s.onerror = function() {
-            response(s);
-        };
-
-        s.src = "https://l2.io/ip.js?var=userip";
-        document.body.appendChild(s);
-    }
-
-    if (/^(interactive|complete)$/i.test(document.readyState)) {
-        trigger();
-    } else {
-        document.addEventListener('DOMContentLoaded', trigger);
-    }
+function GetRedeIp() {
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", 'http://meuip.com/api/meuip.php');
+xmlhttp.send();
+xmlhttp.onload = function(e) {
+  //alert("Seu IP é: "+xmlhttp.response);
+  return xmlhttp.response;
+}
 }
 
-getIp(function (ip) {
-    console.log(ip);
-});
+function getIPs() {
+ return "127.0.0.1";
+};
