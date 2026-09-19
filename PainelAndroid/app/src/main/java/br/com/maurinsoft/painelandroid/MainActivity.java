152db7d8fd33a2b9923316ce3645b83378ec99b7
Marcelo Maurin Martins <marcelomaurinmartins@gmail.com>
2026-09-19 14:42:58 -0300

    feat: adiciona modo imersivo e mantem tela ativa

--- PainelAndroid/app/src/main/java/br/com/maurinsoft/painelandroid/MainActivity.java
+++ PainelAndroid/app/src/main/java/br/com/maurinsoft/painelandroid/MainActivity.java
@@ -12,6 +12,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -93,6 +95,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         preferences = new AppPreferences(this);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        applyImmersiveMode();
 
         initViews();
         restorePanelState();
@@ -100,6 +104,25 @@ public class MainActivity extends AppCompatActivity {
         startClock();
         showConfiguredEndpoint();
         startPanelService();
+    }
+
+    private void applyImmersiveMode() {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            applyImmersiveMode();
+        }
     }
 
     @Override
